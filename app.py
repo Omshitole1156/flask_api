7aa76c1492a2e9539aac959cf0780a38196b4ec3
Omshitole1156 <shitoleom1156@gmail.com>
2025-11-26 07:26:00 +0530

    Bind Flask to 0.0.0.0 and PORT for Render

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask 
-app = Flask(__name__) 
- 
-@app.route("/") 
-def home(): 
-    return "Hello from Cloud PaaS using Render!" 
- 
-if __name__ == "__main__": 
-    app.run() 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Cloud PaaS using Render!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT env var
+    app.run(host="0.0.0.0", port=port)
